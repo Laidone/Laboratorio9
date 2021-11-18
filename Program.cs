a21038d94ab6a3b290d812e58073a8aa452c8f33
Laidone <laidonecarvalho@hotmail.com>
2021-11-18 15:53:39 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace Laboratorio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Código para criar filas de objetos
+            Queue<Object> q = new Queue<Object>();
+            q.Enqueue(".Net Framework / Core"); 
+            q.Enqueue(new Decimal(123.456)); 
+            q.Enqueue(654.321); 
+            Console.WriteLine(q.Dequeue()); 
+            Console.WriteLine(q.Dequeue()); 
+            Console.WriteLine(q.Dequeue());
+
+            //Código para criar filas de inteiros
+            Queue<int> minhaFila = new Queue<int>(); 
+            minhaFila.Enqueue(10);
+            minhaFila.Enqueue(200); 
+            minhaFila.Enqueue(1000); 
+            Console.WriteLine(minhaFila.Dequeue()); 
+            Console.WriteLine(minhaFila.Dequeue()); 
+            Console.WriteLine(minhaFila.Dequeue());
+        }
+
+    }
+}
